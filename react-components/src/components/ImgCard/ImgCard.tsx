392a1b2ddd7c33d4b991da66c54aa3e46eb2af89
batar-btr <btrn86@gmail.com>
2023-04-06 14:51:57 +0300

    feat: add modal component & modal custom hook

--- react-components/src/components/ImgCard/ImgCard.tsx
+++ react-components/src/components/ImgCard/ImgCard.tsx
@@ -1,4 +1,6 @@
+import useModal from '../../hooks/useModal';
 import UnsplashItem from '../../types/UnsplashItemInterface';
+import Modal from '../Modal/Modal';
 import './index.css';
 
 interface ImgCardProps {
@@ -6,14 +8,18 @@ interface ImgCardProps {
 }
 
 export const ImgCard = (props: ImgCardProps) => {
+  const [showModal, toggleModal] = useModal();
   const {
     urls: { small },
     alt_description,
   } = props.info;
   return (
-    <div className="img-card">
+    <div className="img-card" onClick={toggleModal}>
       <h2 className="img-card__title">{alt_description || 'No description'}</h2>
       <img src={small} alt={alt_description} />
+      <Modal isShowing={showModal} hide={toggleModal}>
+        <h1>MODAL BOX</h1>
+      </Modal>
     </div>
   );
 };
---
+++ react-components/src/components/Modal/Modal.tsx
@@ -0,0 +1,27 @@
+import './index.css';
+import ReactDOM from 'react-dom';
+
+type ModalProps = {
+  isShowing: boolean;
+  hide: () => void;
+  children?: React.ReactNode;
+};
+
+const closeModal = (func: () => void) => (e: React.MouseEvent) => {
+  const target = e.target as HTMLElement;
+  if (target.classList.contains('modal-overlay')) {
+    func();
+  }
+};
+
+const Modal = ({ isShowing, hide, children }: ModalProps) =>
+  isShowing
+    ? ReactDOM.createPortal(
+        <div className="modal-overlay" onClick={closeModal(hide)}>
+          {children}
+        </div>,
+        document.body
+      )
+    : null;
+
+export default Modal;
