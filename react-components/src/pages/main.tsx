8e3cd81a08f8ebe3306b02211ecebc267a006462
batar-btr <btrn86@gmail.com>
2023-04-07 16:53:29 +0300

    refactor: rebuild main page & cardlist comp

--- react-components/src/pages/main.tsx
+++ react-components/src/pages/main.tsx
@@ -1,30 +1,16 @@
 import { SearchBar } from '../components/SearchBar/SearchBar';
 import { useState } from 'react';
-import { useSearch } from '../hooks/useSearch';
 import { ImgCardList } from '../components/ImgCardList/ImgCardList';
-import { RotatingLines } from 'react-loader-spinner';
 
 const MainPage = () => {
   const [searchValue, setSearchValue] = useState<string>(localStorage.getItem('value') || 'random');
-  const { loading, error, result } = useSearch('search', searchValue);
   return (
     <>
       <SearchBar setQuery={(value: string) => setSearchValue(value)} />
       <h3 className="help-message">
         Enter a keyword! For example - <q>forest</q>, <q>sea</q>, <q>cats</q> etc.
       </h3>
-      <RotatingLines
-        strokeColor="#fec524"
-        strokeWidth="3"
-        animationDuration="0.75"
-        width="10%"
-        visible={loading}
-      />
-      {!loading && <ImgCardList cards={result} />}
-      {result?.length === 0 && !loading && !error ? (
-        <h1>No images found with this query. Try another keyword!</h1>
-      ) : null}
-      {error && <h2 className="fetch-error">{error.message}</h2>}
+      <ImgCardList searchValue={searchValue} />
     </>
   );
 };
